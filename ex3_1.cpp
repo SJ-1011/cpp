046f96262308564f363adfe875fdad6c9ce46124
SJ-1011 <109647265+SJ-1011@users.noreply.github.com>
2022-09-23 14:36:17 +0900

    Add files via upload

---
+++ ex3_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+struct people
+{
+	char irum[20];
+	int age;
+	char job[20];
+	int pay;
+	void calc_pay()
+	{
+		pay = pay - (pay * 0.033);
+	}
+};
+int main(void)
+{
+	struct people a = { "홍길동", 30, "회사원", 3000000 };
+	a.calc_pay();
+	cout << "실 수령액은 " << a.pay << "입니다." << endl;
+}
